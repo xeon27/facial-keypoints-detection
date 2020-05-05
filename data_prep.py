7049308f1ca1186a63c5be4a6b568572abd5148d
Omkar Dige <oyedige@gmail.com>
2020-05-05 21:39:31 +0530

    updated read_data

--- data_prep.py
+++ data_prep.py
@@ -16,24 +16,45 @@ def read_image(path):
     
     return image
     
-
-def read_data(path, image_path):
-    """ Read the data from csv file """
-    # Read into pandas dataframe
-    df = pd.read_csv(path)
+    
+def read_file(file_path, headers=False):
+    """ Read csv file using python """
+    data = []
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            data.append(line.strip().split(","))
+        
+    # Remove line containing headers
+    if headers:
+        data = data[1:]
+        
+    return data
+    
+    
+def read_data(path, image_dir):
+    """ 
+        Read the data from csv file 
+        Parameters:
+        path (string): path for the csv file
+        image_dir (string): directory containing the image files
+        Returns:
+        data (list): list of dictionaries with two keys - image and key_pts
+    """
+    # Read into a list
+    data_list = read_file(path, headers=True)
     
     # Initialise empty list for image, key points dict
     data = []
     
-    # Iterate over each row of the df
-    for idx in range(df.shape[0]):
-        # Read image name in first column
-        image = df.iloc[idx, 0]
+    # Iterate over each elm of the list
+    for elm in data_list:
+        # Read image name (first elm of the list)
+        image = elm[0]
         # Read image using image name
-        image = read_image(os.path.join(image_path, image))
+        image = read_image(os.path.join(image_dir, image))
         
         # Read corresponding kep points as an array of shape (136,)
-        key_pt = np.array(df.iloc[idx, 1:])
+        key_pt = np.array(elm[1:])
         # Reshape to (68, 2)
         key_pt = np.reshape(key_pt, (-1, 2))
 
@@ -44,9 +65,9 @@ def read_data(path, image_path):
     
 # Class for Facial Keypoints data
 class FacialKeypointsDataset(Dataset):
-    def __init__(self, data_path, image_path):
+    def __init__(self, data_path, image_dir):
         super(FacialKeypointsDataset, self).__init__()
-        self.data = read_data(data_path, image_path)
+        self.data = read_data(data_path, image_dir)
         
     def __len__(self):
         return len(self.data)
